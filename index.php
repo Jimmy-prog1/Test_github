dd2ee45ec30e3e8d1da8c3b0d788e1550603a67a
AndrianinaAnthony <anthonyramanandraisoa@gmail.com>
2025-07-13 10:59:51 +0300

    Alefako tsikelikely amin zay

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Test mety
-    Mino elah sa tsy mino hoe mety le izy
-    Ampiana kely ndray aloha fa tss
-</body>
-</html>
+<?php
+    header("Location:pages/departements.php");
+?>
